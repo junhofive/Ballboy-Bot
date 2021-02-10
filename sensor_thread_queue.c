6b20b9e358760cc0d1aa535bc4e370d13289e869
junhofive <junho5@vt.edu>
2021-02-09 20:25:48 -0500

    Struct definition added; Create and Receive Funcs added.

--- sensor_thread_queue.c
+++ sensor_thread_queue.c
@@ -5,4 +5,41 @@
  *      Author: JO_Desktop
  */
 
+#include "FreeRTOS.h"
+#include "sensor_thread_queue.h"
+#include "timer70.h"
 
+#include "queue.h"
+
+#define QUEUE_LENGTH 10
+/* Static Variable */
+static QueueHandle_t sensor_thread_queue = NULL;
+
+void createSensorThreadQueue() {
+    sensor_thread_queue = xQueueCreate(QUEUE_LENGTH, sizeof(SensorThreadMessage));
+
+    if (sensor_thread_queue == NULL) {
+        //error handling
+    }
+}
+
+void receiveFromSensorThreadQueue(void* receivedMsg) {
+    SensorThreadMessage* someMessage = (SensorThreadMessage* ) receivedMsg;
+
+    if (xQueueReceive(sensor_thread_queue, someMessage, portMAX_DELAY)) {
+
+    }
+    else {
+        //Error, proceed to stop
+    }
+
+}
+
+BaseType_t sendToSensorThreadQueueFromISR(void *targetMessage) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    c_struct *message;
+    message = (c_struct *)pvParameters;
+    if (xQueueSend(sensor_thread_queue, (void *) &message, (portTickType) 0) != pdPASS) {
+
+    }
+}
--- sensor_thread_queue.h
+++ sensor_thread_queue.h
@@ -8,6 +8,20 @@
 #ifndef SENSOR_THREAD_QUEUE_H_
 #define SENSOR_THREAD_QUEUE_H_
 
+#include <FreeRTOS.h>
+#include <queue.h>
 
+#define MESSAGE_LENGTH 20
+
+typedef struct SensorThreadMessage {
+    char  message_type[MESSAGE_LENGTH]
+    int   value;
+} SensorThreadMessage;
+
+void createSensorThreadQueue();
+
+void receiveFromSensorThreadQueue(void* receivedMsg);
+
+BaseType_t sendToSensorThreadQueueFromISR(void *targetMessage);
 
 #endif /* SENSOR_THREAD_QUEUE_H_ */

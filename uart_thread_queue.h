0b0a08694e04676d8f5f2c2f4b6435589a965192
coolprogrammer25 <60118112+coolprogrammer25@users.noreply.github.com>
2021-02-09 21:51:41 -0500

    Edited the uart thread queues

--- uart_thread_queue.h
+++ uart_thread_queue.h
@@ -1,13 +1,20 @@
 /*
  * uart_thread_queue.h
  *
- *  Created on: Feb 6, 2021
- *      Author: JO_Desktop
+ *
  */
 
 #ifndef UART_THREAD_QUEUE_H_
 #define UART_THREAD_QUEUE_H_
 
+typedef struct UART_Thread_Queue{
+    char message_queue[10];
+    int val;
+}UART_Thread_Queue;
 
+
+void create_UART_Queue();
+void read_from_queue(void* retrievedMsg);
+BaseType_t write_queue(void *outputMessage);
 
 #endif /* UART_THREAD_QUEUE_H_ */
